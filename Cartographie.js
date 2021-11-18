b16da82977f4951369a1f0458e0d89488dc9662c
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-18 17:46:17 +0100

    Install package.json and creation components and pages

---
+++ Cartographie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cartographie = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Cartographie;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+        <div
+        id="NavBar"
+        className=""
+        >
+        <div id="logo" className=""
+        >   
+        <img src="" alt="img-logo" className="" />
+        
+        </div>
+        </>
+    );
+};
+
+export default Navbar;
